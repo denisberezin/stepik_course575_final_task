9653996b6fb750d23ba5656ebc84a1ff971099f3
Denis Berezin <denis.berezin@ringcentral.com>
2022-06-16 22:37:01 +0300

    login page handling added

---
+++ pages/locators.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators:
+    LOGIN_FORM_EMAIL = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_FORM_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
+    REGISTER_FORM_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTER_FORM_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTER_FORM_CONFIRM_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password2")
